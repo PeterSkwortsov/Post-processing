05a450617746d894d72b20bf21496ea620c59981
Peter <skwortsow.petia@yandex.ru>
2025-07-22 16:58:53 +0300

    Физика

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,69 +1,62 @@
 import { OrbitControls } from "@react-three/drei";
-import { useGLTF, Environment, Float, PresentationControls, ContactShadows, Html, Text } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { Debug, Physics, RigidBody } from "@react-three/rapier";
 
 export default function Experience() {
      
-    const computer = useGLTF(
-      "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
-    );
+
 
     return (
       <>
-        <color attach="background" args={["#312b2b"]} />
+        <Perf position="top-left" />
+
+        <color attach="background" args={["#9ef1f1"]} />
         <OrbitControls makeDefault />
 
-        <PresentationControls
-          global={false}
-          rotation={[0.15, 0.2, 0]}
-          polar={[-0.4, 0.2]}
-          azimuth={[-1, 0.75]}
-          // config={{ mass: 2, tension: 400 }}
-          // snap={{ mass: 4, tension: 400 }}
-        >
-          <Float rotationIntensity={0.4}>
-            <rectAreaLight
-              width={2.5}
-              height={1.65}
-              intensity={65}
-              color="#eca777"
-              rotation={[-0.1, Math.PI, 0]}
-              position={[0, 0.55, -1.15]}
-            />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[1, 2, 3]} castShadow intensity={4.5} />
 
-            <primitive
-              object={computer.scene}
-              scale={1.6}
-              position-y={-1.6}
-              position-z={1}
-              position-x={-0.2}
+        <Physics>
+          <Debug />
+
+          <RigidBody>
+            <mesh position={[4, 2, 0]} rotation-y={Math.PI * 0.5} castShadow>
+              <boxGeometry args={[3, 3, 0.3]} />
+              <meshStandardMaterial color="orange" />
+            </mesh>
+          </RigidBody>
+          <RigidBody>
+            <mesh castShadow position={[2, 0, 3]}>
+              <boxGeometry args={[1, 1, 1]} />
+              <meshStandardMaterial color="multicolored" />
+            </mesh>
+          </RigidBody>
+          <RigidBody colliders="ball">
+            <mesh position={[0, 2, 0]} castShadow>
+              <sphereGeometry args={[0.8, 32, 32]} />
+              <meshStandardMaterial color="hotpink" />
+            </mesh>
+          </RigidBody>
+
+          <RigidBody type="fixed">
+            <mesh position={[0, -2, 0]} receiveShadow>
+              <boxGeometry args={[10, 0.5, 10]} />
+              <meshStandardMaterial color="green" />
+            </mesh>
+          </RigidBody>
+
+          <RigidBody colliders="trimesh">
+            <mesh
+              castShadow
+              position={[0, 2, 0]}
+              rotation={[Math.PI * 0.1, 0, 0]}
+              receiveShadow
             >
-              <Html
-                transform
-                wrapperClass="htmlScreen"
-                distanceFactor={1.17}
-                position={[0, 1.56, -1.4]}
-                rotation-x={-0.256}
-              >
-                <iframe occlude src="https://borsch-art.ru/"></iframe>
-              </Html>
-              <Text
-              font="./Forum.woff"
-              fontWeight={100}
-                position={[2, 1.75, 0]}
-                fontSize={0.25}
-                rotation-y={-1.15}
-                maxWidth={1.5}
-                textAlign="center"
-              >
-                Тут может быть ваш сайт
-              </Text>
-            </primitive>
-          </Float>
-        </PresentationControls>
-        <ContactShadows position-y={-2.0} blur={2} />
-
-        {/* <ambientLight intensity={1.5} /> */}
-        <directionalLight position={[3, 2, 1]} intensity={10.5} />
+              <torusGeometry args={[1.2, 0.2, 16, 32]} />
+              <meshStandardMaterial color="greenyellow" />
+            </mesh>
+          </RigidBody>
+        </Physics>
       </>
     );
 }
