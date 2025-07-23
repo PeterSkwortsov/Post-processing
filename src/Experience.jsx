8007c3d76e68cbaae4c65088162d7e765e79c84a
Peter <skwortsow.petia@yandex.ru>
2025-07-23 13:15:41 +0300

    немного анимации

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,6 +2,8 @@ import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import { BallCollider, CuboidCollider, Debug, Physics, RigidBody } from "@react-three/rapier";
 import { useRef } from "react";
+import * as THREE from 'three'
+import { useFrame } from "@react-three/fiber";
 export default function Experience() {
      
   const cube = useRef();
@@ -9,6 +11,30 @@ export default function Experience() {
     cube.current.applyImpulse({ x: -0.5, y: 4.6, z: Math.random() - 0.5 });
     cube.current.applyTorqueImpulse({ x: 0, y: 1.5, z: 0.1 });
   };
+
+  
+  const twister = useRef();
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+
+    const eulerRotation = new THREE.Euler(0, time, 0)
+    const quaternionRotation = new THREE.Quaternion()
+    quaternionRotation.setFromEuler(eulerRotation)
+    twister.current.setNextKinematicRotation(quaternionRotation);
+
+
+  const angale = time*0.5;
+  const x = Math.sin(angale * 4) * 5;
+  const z = Math.cos(angale) * 5;
+  twister.current.setNextKinematicTranslation({x:x, y: 0.4, z: z});
+
+  })
+
+  const collisionEnter = () => {
+    console.log('толчок')
+  }
+
     return (
       <>
         <Perf position="top-left" />
@@ -24,25 +50,24 @@ export default function Experience() {
 
           <RigidBody>
             <mesh position={[4, 2, 0]} rotation-y={Math.PI * 0.5} castShadow>
-              <boxGeometry args={[3, 3, 0.3]} />
+              <boxGeometry args={[4, 4, 0.3]} />
               <meshStandardMaterial color="orange" />
             </mesh>
           </RigidBody>
           <RigidBody
             ref={cube}
-            position={[0, 5, 0]}
-            // gravityScale={0.08}
-            // friction={0.2}
+            position={[-2, 5, 3]}
+            gravityScale={5.08}
+            friction={0.8}
+            mass={1}
           >
             <mesh castShadow onClick={cubeJump}>
-              <boxGeometry args={[1, 1, 1]} />
+              <boxGeometry args={[0.7, 1, 0.4]} />
               <meshStandardMaterial color="red" />
             </mesh>
           </RigidBody>
           <RigidBody colliders={false}>
-            <BallCollider args={[1.5]} mass={1}
-            
-            />
+            <BallCollider args={[1.5]} mass={1} />
             <mesh position={[0, 4, 0]} castShadow>
               <sphereGeometry args={[0.8, 32, 32]} />
               <meshStandardMaterial color="hotpink" />
@@ -57,16 +82,33 @@ export default function Experience() {
             </mesh>
           </RigidBody>
 
-          <RigidBody colliders="trimesh">
+          <RigidBody
+            colliders="trimesh"
+            gravityScale={5.08}
+            friction={0.8}
+            mass={1}
+          >
             {/* <CuboidCollider args={[1, 1, 1]} /> */}
             <mesh
               castShadow
-              position={[0, 2, 0]}
+              position={[-1, 5, 0]}
               rotation={[Math.PI * 0.1, 0, 0]}
               receiveShadow
             >
               <torusGeometry args={[1.2, 0.2, 16, 32]} />
               <meshStandardMaterial color="greenyellow" />
+            </mesh>
+          </RigidBody>
+          <RigidBody
+            position={[0, 0.4, 0]}
+            friction={0} // трение
+            type="kinematicPosition"
+            ref={twister}
+            onCollisionEnter={collisionEnter}
+          >
+            <mesh castShadow scale={[0.3, 0.3, 3]}>
+              <boxGeometry />
+              <meshStandardMaterial color="blue" />
             </mesh>
           </RigidBody>
         </Physics>
